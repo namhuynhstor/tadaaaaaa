67ac7b6431ecbc8c21de3f4aa7cbc4dd72237370
Nhat Nam Huynh <129588364+namhuynhstor@users.noreply.github.com>
2024-02-13 17:14:14 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from '/src/App.tsx'
-import '/src/index.css'
+import App from 'src/App.tsx'
+import 'src/index.css'
 
 ReactDOM.render(
   <React.StrictMode>
