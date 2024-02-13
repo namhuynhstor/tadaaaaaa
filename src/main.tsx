5aab7583ae4b8bb4d4a248856e4a1b41ca66054a
Nhat Nam Huynh <129588364+namhuynhstor@users.noreply.github.com>
2024-02-13 17:18:57 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom'
 import App from 'src/App.tsx'
 import 'src/index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
