a8b13a6dad7d8e21e8981d040810cd53641cf7b8
Nam Huynh <bennohuynh@Nams-MacBook-Pro.local>
2024-02-13 16:20:02 +0700

    Your commit message here

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Change import statement
+import App from '/Users/bennohuynh/Desktop/will-you-be-my-valentine-main/src/App.tsx';
+import '/Users/bennohuynh/Desktop/will-you-be-my-valentine-main/src/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
